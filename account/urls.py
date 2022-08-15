6282a359e0abd6157f3ff9c2ef25ba69d1c0a605
raj3k <rajkosky@gmail.com>
2022-08-15 21:59:29 +0200

    url to logout

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from .forms import CustomLoginForm
 
 
 urlpatterns = [
-    path('login/', LoginView.as_view(authentication_form=CustomLoginForm)),
+    path('login/', LoginView.as_view(authentication_form=CustomLoginForm), name='login'),
+    path('logout/', LogoutView.as_view()),
     path('', include('django.contrib.auth.urls')),
 ]
