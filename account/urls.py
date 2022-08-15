1b69b8d7658163cff4b255b02e152c0d2a1d59c0
raj3k <rajkosky@gmail.com>
2022-08-15 21:22:57 +0200

    login page added

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from . import views
+from django.contrib.auth.views import LoginView
+from .forms import CustomLoginForm
 
 
 urlpatterns = [
+    path('login/', LoginView.as_view(authentication_form=CustomLoginForm)),
     path('', include('django.contrib.auth.urls')),
 ]
