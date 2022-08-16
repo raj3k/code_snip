8d66d23803a040d129acb5d7a77b4d98e50ec453
raj3k <rajkosky@gmail.com>
2022-08-16 22:05:11 +0200

    url connected with dashboard view

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard'),
 ]
