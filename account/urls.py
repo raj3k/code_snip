e406c0bd4ae41dc6d89960ca2e7d863c53dd6a79
raj3k <rajkosky@gmail.com>
2022-08-16 14:28:20 +0200

    custom login view removed from urlpatterns

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path, include
-from django.contrib.auth.views import LoginView, LogoutView
-from .forms import CustomLoginForm
 
 
 urlpatterns = [
-    path('login/', LoginView.as_view(authentication_form=CustomLoginForm), name='login'),
-    path('logout/', LogoutView.as_view()),
     path('', include('django.contrib.auth.urls')),
 ]
