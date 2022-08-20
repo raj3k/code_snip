8212bdc0323973b02afbf8e8520dd4271f31b92c
raj3k <rajkosky@gmail.com>
2022-08-20 11:22:23 +0200

    implemented profile creation using django signals

--- account/models.py
+++ account/models.py
@@ -4,12 +4,8 @@ from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
+    bio = models.TextField(default="404 bio not found", blank=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
 
     def __str__(self):
         return f'Profile of {self.user.username}'
-
-    def clean(self):
-        if not self.bio:
-            self.bio = "404 bio not found"
