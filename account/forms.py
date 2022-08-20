9696faddd36aa314f32bdd91af60456fb57d1a2e
raj3k <rajkosky@gmail.com>
2022-08-20 17:33:17 +0200

    added clean_email to form to prevent users from using an existing email

--- account/forms.py
+++ account/forms.py
@@ -17,6 +17,12 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError("Passwords don't match.")
         return cd['password2']
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
+
 
 class UserEditForm(forms.ModelForm):
     class Meta:
@@ -24,10 +30,11 @@ class UserEditForm(forms.ModelForm):
         fields = ['first_name', 'last_name', 'email']
 
     def clean_email(self):
-        email = self.cleaned_data['email']
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('Email already in use!')
-        return email
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError(' Email already in use.')
+        return data
 
 
 class ProfileEditForm(forms.ModelForm):
