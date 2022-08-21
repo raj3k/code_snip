a938ba83d37faf81477e6c4bbc0f3e222319ae9f
raj3k <rajkosky@gmail.com>
2022-08-21 09:21:43 +0200

    created a model to store code snippets

--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.conf import settings
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
-# Create your models here.
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+
+
+class Snippet(models.Model):
+    #     id UUID
+    user = models.ForeignKey(
+        settings.USER_AUTH_MODEL,
+        related_name='snippets_created',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(
+        max_length=200,
+        blank=True
+    )
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES,
+        default='python',
+        max_length=100
+    )
+    style = models.CharField(
+        choices=STYLE_CHOICES,
+        default='friendly',
+        max_length=100
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
