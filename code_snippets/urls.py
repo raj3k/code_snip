e519b35f560a1030179ecbba0cda03c3870f82b6
raj3k <rajkosky@gmail.com>
2022-08-21 17:36:11 +0200

    added snippets' path in project urls

--- code_snippets/urls.py
+++ code_snippets/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
+    path('snippets/', include('snippets.urls', namespace='snippets')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
