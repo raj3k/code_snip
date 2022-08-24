bbc1dafa02b6fcac9c21df22b396b33b6d766a17
raj3k <rajkosky@gmail.com>
2022-08-24 22:05:41 +0200

    url responsible for liking snippets

--- snippets/urls.py
+++ snippets/urls.py
@@ -6,4 +6,5 @@ app_name = 'snippets'
 urlpatterns = [
     path('create/', views.snippet_create, name='create'),
     path('detail/<int:id>/<slug:slug>/', views.snippet_detail, name='detail'),
+    path('like/', views.snippet_like, name='like'),
 ]
