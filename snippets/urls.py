e5a9ba4624725b9b0a8d0c85cad851cc2cdadaab
raj3k <rajkosky@gmail.com>
2022-08-27 08:49:35 +0200

    URL pattern for list of snippets

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'snippets'
 
 urlpatterns = [
+    path('', views.snippet_list, name='list'),
     path('create/', views.snippet_create, name='create'),
     path('detail/<int:id>/<slug:slug>/', views.snippet_detail, name='detail'),
     path('like/', views.snippet_like, name='like'),
