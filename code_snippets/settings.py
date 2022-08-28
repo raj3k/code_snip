fcfc0513c1e1ce0e16b2e4717588e8ded4b17d0b
raj3k <rajkosky@gmail.com>
2022-08-28 16:01:57 +0200

    taggit app installed

--- code_snippets/settings.py
+++ code_snippets/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'widget_tweaks',
     'social_django',
     'snippets.apps.SnippetsConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -88,8 +89,12 @@ WSGI_APPLICATION = 'code_snippets.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
     }
 }
 
