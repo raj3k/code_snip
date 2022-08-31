4cd078d6b36e43efe86880c856f281694d345c3d
raj3k <rajkosky@gmail.com>
2022-08-31 16:56:36 +0200

    added snippet list path to urls

--- code_snippets/urls.py
+++ code_snippets/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from snippets.views import snippet_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
     path('snippets/', include('snippets.urls', namespace='snippets')),
+    path('', snippet_list, name="snippets_list"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
